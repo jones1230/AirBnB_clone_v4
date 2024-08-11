a23e33097cd3d194e48ad2f75a87094a2e581d0e
Jesse Yeboah <jesseyeboah16@gmail.com>
2024-08-11 20:00:11 +0000

    add 1-hbnb.js, edit 1-hbnb.html

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    let selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).attr('data-id');
+        const amenityName = $(this).attr('data-name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        const amenitiesList = Object.values(selectedAmenities).join(', ');
+        $('.amenities h4').text(amenitiesList);
+    });
+});
